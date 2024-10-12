61083ddf05631433442f5aacf774f1e991ebfebf
jnos0003 <jnos0003@student.monash.edu>
2024-10-12 13:46:21 +1100

    Update vega_lite_vis.js

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,14 @@
 var vg_1 = "bushfires_in_australia.vg.json";
-vegaEmbed("#map_aus", vg_1).then(function(result){
+vegaEmbed("#bushfireMapAus", vg_1).then(function(result){
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "Native_area_burned.vg.json";
+vegaEmbed("#NativefireBarAus", vg_2).then(function(result){
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "Native_area_burned_2.vg.json";
+vegaEmbed("#NativefireBarAus2", vg_3).then(function(result){
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
